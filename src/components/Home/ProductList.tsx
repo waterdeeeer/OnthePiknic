11e9236f58b42d4210e6b3a7360693fd04c23f1f
potatojoayo <potatojoayo@gmail.com>
2021-05-02 15:36:12 +0900

    pre commit

--- src/components/Home/ProductList.tsx
+++ src/components/Home/ProductList.tsx
@@ -1,32 +1,32 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../../store";
+import {useSelector} from "react-redux";
+import {RootState} from "../../store";
 
 import Container from "../style/Container";
 import ProductListColumn from "./ProductListColumn";
 interface ProductListProps {
-  categoryName: string;
+	categoryName: string;
 }
 const ProductList: React.FC<ProductListProps> = () => {
-  const allProduct = useSelector((state: RootState) => state.db).all!;
-  let totalColumnNumber = 4;
-  let i = 0;
-  const renderedColumns: JSX.Element[] = [];
-  for (; i < columnNumber; i++) {
-    renderedColumns.push(
-      <ProductListColumn
-        products={allProduct}
-        totalColumnCount={totalColumnNumber}
-        columnNumber={i}
-        key={i}
-      ></ProductListColumn>
-    );
-  }
-  return (
-    <Container display="flex" width="90vw" height="90vh" margin="30px 0 0 0">
-      {renderedColumns}
-    </Container>
-  );
+	const allProduct = useSelector((state: RootState) => state.db).all!;
+	let totalColumnNumber = 4;
+	let i = 0;
+	const renderedColumns: JSX.Element[] = [];
+	for (; i < totalColumnNumber; i++) {
+		renderedColumns.push(
+			<ProductListColumn
+				products={allProduct}
+				totalColumnCount={totalColumnNumber}
+				columnNumber={i}
+				key={i}
+			></ProductListColumn>
+		);
+	}
+	return (
+		<Container display="flex" width="90vw" height="90vh" margin="30px 0 0 0">
+			{renderedColumns}
+		</Container>
+	);
 };
 
 export default ProductList;
--- src/components/Home/ProductListColumn.tsx
+++ src/components/Home/ProductListColumn.tsx
@@ -1,30 +1,30 @@
 import React from "react";
-import { Product } from "../../types";
+import {Product} from "../../types";
 import Container from "../style/Container";
 import ProductListItem from "./ProductListItem";
 
 interface ProductListColumnProps {
-  products: Product[];
-  totalColumnCount: number;
-  columnNumber: number;
+	products: Product[];
+	totalColumnCount: number;
+	columnNumber: number;
 }
 
 const ProductListColumn: React.FC<ProductListColumnProps> = ({
-  products,
-  totalColumnCount,
-  columnNumber,
+	products,
+	totalColumnCount,
+	columnNumber,
 }) => {
-  const filteredItems = products.filter(
-    (_, index) => columnNumber === index % totalColumnCount
-  );
-  const renderedItems = filteredItems.map((item, i) => {
-    return <ProductListItem product={item} key={i}></ProductListItem>;
-  });
-  return (
-    <Container width={`calc(${100 / totalColumnCount}% - 10px)`margin="30px"}>
-      {renderedItems}
-    </Container>
-  );
+	const filteredItems = products.filter(
+		(_, index) => columnNumber === index % totalColumnCount
+	);
+	const renderedItems = filteredItems.map((item, i) => {
+		return <ProductListItem product={item} key={i}></ProductListItem>;
+	});
+	return (
+		<Container width={`calc(${100 / totalColumnCount}% - 10px)`} margin="30px">
+			{renderedItems}
+		</Container>
+	);
 };
 
 export default ProductListColumn;
